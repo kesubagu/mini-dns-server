2ec5632d93ac247641d840c7ed9c2b630f121cd9
Keshav Bagla <keshavbagla@gmail.com>
2026-02-16 23:01:39 -0500

    A messy control server

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A small DNS server that NXDOMAINs anything in the blocklist
+```
+curl -XPOST -H 'Content-Type: application/json' http://localhost:8080/blocklist # -d '{"domain_name": "gooogle.ads.com" }'
+curl -XGET -H 'Content-Type: application/json' http://localhost:8080/blocklist
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::routing::{get, post};
+use axum::{Json, Router};
 use dashmap::DashMap;
 use hickory_proto::op::{Message, MessageType, ResponseCode};
 use hickory_proto::serialize::binary::BinDecodable;
+use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -8,6 +13,7 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 use tokio::net::UdpSocket;
+use tokio::sync::RwLock;
 use tokio::time::timeout;
 
 type DynError = Box<dyn std::error::Error + Send + Sync>;
@@ -15,26 +21,48 @@ const MAX_DNS_PACKET_SIZE: usize = 4096;
 const UPSTREAM_TIMEOUT: Duration = Duration::from_secs(3);
 const DEFAULT_TTL_SECS: u64 = 30;
 const BLOCKLIST_PATH: &str = "blocked_domains.txt";
+const HTTP_CONTROL_ADDR: &str = "127.0.0.1:8080";
 
 struct CacheEntry {
     response: Vec<u8>,
     expires_at: Instant,
 }
 
+struct AppState {
+    block_list: Arc<RwLock<HashSet<String>>>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), DynError> {
     let cache = Arc::new(DashMap::<String, CacheEntry>::new());
-    let blocked_domains = Arc::new(load_blocked_domains(BLOCKLIST_PATH)?);
+    let blocked_domains = Arc::new(RwLock::new(load_blocked_domains(BLOCKLIST_PATH)?));
     let listener_addr = "0.0.0.0:53";
     let upstream_dns_server = "1.1.1.1:53";
 
     let socket = Arc::new(UdpSocket::bind(listener_addr).await?);
     println!("DNS cache is running on {}", listener_addr);
     println!(
-        "Loaded {} blocked domains from {}",
-        blocked_domains.len(),
-        BLOCKLIST_PATH
+        "HTTP control API is running on http://{}",
+        HTTP_CONTROL_ADDR
     );
+    let blocked_count = blocked_domains.read().await.len();
+    println!(
+        "Loaded {} blocked domains from {}",
+        blocked_count, BLOCKLIST_PATH
+    );
+
+    let app_state = Arc::new(AppState {
+        block_list: blocked_domains.clone(),
+    });
+
+    let app = Router::new()
+        .route("/blocklist", get(get_blocklist))
+        .route("/blocklist", post(put_blocklist))
+        .with_state(app_state);
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await?;
 
     let mut buf = vec![0u8; MAX_DNS_PACKET_SIZE];
     loop {
@@ -46,16 +74,15 @@ async fn main() -> Result<(), DynError> {
         let upstream = upstream_dns_server.to_string();
 
         tokio::spawn(async move {
-            if let Err(err) =
-                handle_request(
-                    socket_clone,
-                    req_bytes,
-                    addr,
-                    cache_clone,
-                    blocked_domains_clone,
-                    upstream,
-                )
-                .await
+            if let Err(err) = handle_request(
+                socket_clone,
+                req_bytes,
+                addr,
+                cache_clone,
+                blocked_domains_clone,
+                upstream,
+            )
+            .await
             {
                 eprintln!("Failed to handle request from {addr}: {err}");
             }
@@ -63,26 +90,55 @@ async fn main() -> Result<(), DynError> {
     }
 }
 
+#[derive(Serialize)]
+struct BlocklistResponse {
+    blocklist: Vec<String>,
+}
+
+async fn get_blocklist(State(state): State<Arc<AppState>>) -> Json<BlocklistResponse> {
+    let reader = state.block_list.read().await;
+    let mut blocklist: Vec<String> = reader.iter().cloned().collect();
+    blocklist.sort();
+    Json(BlocklistResponse {
+        blocklist: blocklist,
+    })
+}
+
+#[derive(Deserialize)]
+struct BlocklistRequest {
+    domain_name: String,
+}
+
+async fn put_blocklist(
+    State(state): State<Arc<AppState>>,
+    Json(request): Json<BlocklistRequest>,
+) -> StatusCode {
+    let mut writer = state.block_list.write().await;
+    println!("Adding {} to blocklist", request.domain_name);
+    writer.insert(request.domain_name);
+    StatusCode::ACCEPTED
+}
+
 async fn handle_request(
     socket: Arc<UdpSocket>,
     req_bytes: Vec<u8>,
     src_addr: SocketAddr,
     cache: Arc<DashMap<String, CacheEntry>>,
-    blocked_domains: Arc<HashSet<String>>,
+    blocked_domains: Arc<RwLock<HashSet<String>>>,
     upstream_dns_server: String,
 ) -> Result<(), DynError> {
-    println!("Received DNS request from {}", src_addr);
-
     let request = Message::from_bytes(&req_bytes)?;
     if request.message_type() != MessageType::Query {
         return Ok(());
     }
 
-    if is_blocked(&request, &blocked_domains) {
+    let blocklist_guard = blocked_domains.read().await;
+    if is_blocked(&request, &blocklist_guard) {
         let nxdomain = build_nxdomain_response(&request)?;
         socket.send_to(&nxdomain, src_addr).await?;
         return Ok(());
     }
+    drop(blocklist_guard);
 
     let cache_key = cache_key(&request);
     if let Some(key) = &cache_key {
